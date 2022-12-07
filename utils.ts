8e85df4b06bd58e9444aeddefd772852d6c75ee7
Kazuki Taninaka <onelive555@gmail.com>
2022-12-07 15:08:59 +0100

    change date format

--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 export function convertDate(d: string): string {
   const date = new Date(d);
-  return `${date.getFullYear()}年${
+  return `${date.getFullYear()}/${
     date.getMonth() + 10
-  }月${date.getDate()}日 ${date.getHours()}時${date.getMinutes()}分`;
+  }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
 }
