61645ab8672e84fd770e7a6b7d7c7261f8944fa7
Kazuki Taninaka <onelive555@gmail.com>
2022-12-07 17:18:19 +0100

    introduce src folder

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <div>
-      <div style={{ borderBottom: "1px solid #000" }}>
+      <header style={{ borderBottom: "1px solid #000" }}>
         <h1 style={{ margin: "20px", fontSize: "2rem", fontWeight: "bold" }}>
           <Link href="/">blog.azoookid.dev</Link>
         </h1>
-      </div>
-      <div style={{ maxWidth: "750px", margin: "auto" }}>{children}</div>
+      </header>
+      <main style={{ maxWidth: "750px", margin: "auto" }}>{children}</main>
     </div>
   );
 }
