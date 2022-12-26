53c8af5545c429206c096f03edb5e6bd18c278f8
Kazuki Taninaka <onelive555@gmail.com>
2022-12-26 21:26:33 +0100

    add ArticleCard.css.ts

---
+++ src/components/top/ArticleCard.css.ts
@@ -0,0 +1,12 @@
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  margin: "20px",
+});
+
+export const title = style({
+  fontSize: "1.5rem",
+  textDecorationLine: "underline",
+  cursor: "pointer",
+  marginBottom: "10px",
+});
--- src/components/top/ArticleCard.tsx
+++ src/components/top/ArticleCard.tsx
@@ -1,19 +1,11 @@
 import Link from "next/link";
+import * as styles from "./ArticleCard.css";
 
 export default function ArticleCard({ title, createdAt, id }) {
   return (
-    <div style={{ margin: "20px" }}>
+    <div className={styles.container}>
       <Link href={`article/${id}`} passHref>
-        <div
-          style={{
-            fontSize: "1.5rem",
-            textDecorationLine: "underline",
-            cursor: "pointer",
-            marginBottom: "10px",
-          }}
-        >
-          {title}
-        </div>
+        <div className={styles.title}>{title}</div>
       </Link>
       <div>{createdAt}公開</div>
     </div>
