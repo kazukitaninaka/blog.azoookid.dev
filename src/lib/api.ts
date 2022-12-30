c3553c34cf2a0713df999be156afa5a3fc198036
Kazuki Taninaka <onelive555@gmail.com>
2022-12-30 21:44:08 +0100

    fix date bug

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -42,6 +42,6 @@ export function getAllArticles(fields: string[] = []) {
   const posts = slugs
     .map((slug) => getArticleBySlug(slug, fields))
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+    .sort((post1, post2) => (post1.createdAt > post2.createdAt ? -1 : 1));
   return posts;
 }
