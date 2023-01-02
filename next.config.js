f4d8622bca2ca5097dafdf310053ca274a4876f4
Kazuki Taninaka <onelive555@gmail.com>
2023-01-02 22:01:27 +0100

    modify next.js config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["s3.us-west-2.amazonaws.com", "www.notion.so"],
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
