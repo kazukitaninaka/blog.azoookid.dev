34ea551947f2be2910e6c951945f770ef845601c
Kazuki Taninaka <onelive555@gmail.com>
2023-01-02 21:17:07 +0100

    add footer

--- src/components/common/Layout.css.ts
+++ src/components/common/Layout.css.ts
@@ -1,8 +1,17 @@
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
+  display: "grid",
+  gridTemplateRows: "auto 1fr auto",
+  gridTemplateColumns: "100%",
+  minHeight: "100vh",
+  gap: "30px",
+});
+
+export const mainContainer = style({
+  width: "100%",
   maxWidth: "750px",
-  margin: "auto",
+  margin: "0 auto",
   "@media": {
     "screen and (max-width: 750px)": {
       margin: "0 10px",
@@ -19,3 +28,12 @@ export const linkContainer = style({
   fontSize: "2rem",
   fontWeight: "bold",
 });
+
+export const footer = style({
+  padding: "20px",
+  borderTop: "1px solid #000",
+  display: "grid",
+  placeItems: "center",
+});
+
+export const emoji = style({});
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -3,13 +3,16 @@ import * as styles from "./Layout.css";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.linkContainer}>
           <Link href="/">blog.azoookid.dev</Link>
         </div>
       </header>
-      <main className={styles.container}>{children}</main>
+      <main className={styles.mainContainer}>{children}</main>
+      <footer className={styles.footer}>
+        <div>This website is built with Next.js</div>
+      </footer>
     </div>
   );
 }
